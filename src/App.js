99afd495c787d2f51719001eed8f7ab7c09a4296
Natnael Gebremichael <natnael.gebremichael@datacom.co.nz>
2021-12-23 15:06:22 +1300

    Initial commit, Setup - react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+
+  const title = 'what\'s up';
+  const likes = 50;
+  const link  = "https://www.google.com";
+
+  return (
+    <div className="App">
+      <div className="content">
+      
+        <h1>{title}</h1>
+        <p>Liked {likes} times </p>
+        <a href={link}>Google Site</a>
+
+        </div> 
+    </div>
+  );
+}
+
+export default App;
