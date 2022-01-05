9044b5a7bee0b4966d2ce6b47734eb102496a836
Natnael Gebremichael <natnael.gebremichael@datacom.co.nz>
2022-01-05 20:18:10 +1300

    added click event, and used hooks for variables

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const handleClick = (e) => {
+        setName('kibrom');
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
